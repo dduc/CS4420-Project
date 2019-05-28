445894e0d359cf9abca5e36caaf22b987ec6298c
dduc <31668550+dduc@users.noreply.github.com>
2019-05-28 10:06:41 -0700

    readability

--- power_prediction.py
+++ power_prediction.py
@@ -1,11 +1,18 @@
-# This program is for power prediction based off
-# of 22 days worth of data, at 5 second intervals,
-# at each 5 seconds looking at temperature,
-# humidity, etc. This approach uses a
-# multivariate LSTM network
+'''
+ Main Author: Dirk Duclos
 
+ CSV Formatting: Sean Nickell
 
-#imports for tensorflow,numpy,pandas using keras deep learning library
+ Last Modified: 5-28-19
+
+ Purpose: This program is for power consumption prediction
+ based off of 22 days worth of data, at 5 second
+ intervals,each interval holds temperature, humidity,
+ solar, and wind data. This approach uses a multivariate
+ LSTM network for prediction.
+'''
+
+#imports for tensorflow,numpy,and pandas using keras deep learning library
 import numpy as np
 from numpy import hstack
 
@@ -29,12 +36,10 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 def split_all_data(allData, t_steps):
 	X,y = list(),list()
 	for i in range(len(allData)):
-		# find the end of this pattern
 		end_ix = i + t_steps
-		# check if we are beyond the dataset
+		# see if at end of dataset
 		if end_ix > 345560:
 			break
-		# gather input and output parts of the pattern
 		seq_x, seq_y = allData[i:end_ix, :-1], allData[end_ix-1, -1]
 		X.append(seq_x)
 		y.append(seq_y)
@@ -42,9 +47,9 @@ def split_all_data(allData, t_steps):
 	return np.array(X), np.array(y)
 
 # --------------------------- DATA PREP FOR LSTM NETWORK ---------------------------------- #
-
+# This code below is commented out because data prep has already been done
 '''
-# import CSV file data, leaving out last day (22) and prev day (21) until model is finished and accurate
+# import CSV file data, leaving out last day (22) and prev day (21) for finishing model accuracy based on real data
 humidity_data = pd.read_csv("HumidityS.csv", skiprows = 0,usecols = ['Day 1','Day 2','Day 3','Day 4','Day 5','Day 6','Day 7','Day 8','Day 9','Day 10','Day 11','Day 12','Day 13','Day 14','Day 15','Day 16','Day 17','Day 18','Day 19','Day 20'])
 solar_data = pd.read_csv("SolarS.csv",skiprows = 0,usecols = ['Day1','Day2','Day3','Day4','Day5','Day6','Day7','Day8','Day9','Day10','Day11','Day12','Day13','Day14','Day15','Day16','Day17','Day18','Day19','Day20'])
 temp_data = pd.read_csv("TemperatureS.csv",skiprows = 0, usecols = ['Day1','Day2','Day3','Day4','Day5','Day6','Day7','Day8','Day9','Day10','Day11','Day12','Day13','Day14','Day15','Day16','Day17','Day18','Day19','Day20'])
@@ -86,8 +91,7 @@ power_seq = power_seq.flatten('F')
 
 time_of_day_seconds_seq = np.array(time_of_day_seconds)
 
-
-# shape sequences to  vectors
+# shape sequences to vectors
 humidity_seq = humidity_seq.reshape(len(humidity_seq),1)
 temp_seq = temp_seq.reshape(len(temp_seq),1)
 wind_seq = wind_seq.reshape(len(wind_seq),1)
@@ -101,26 +105,25 @@ time_of_day_seconds_seq = time_of_day_seconds_seq.reshape(len(time_of_day_second
 all_data = np.hstack((time_of_day_seconds_seq,humidity_seq,temp_seq,wind_seq,solar_seq,power_seq))
 
 # format all_data for LSTM network (3 dimensions)
-# Data was formatted using EXCEL formula
+# Data is in final format using EXCEL formula
 '''
 
-# read in newly formatted dataset
+# read in newly formatted dataset for prediction
 print("Reading in Data...")
-data = pd.read_csv('all_data.csv',skiprows=0,header = 0,index_col = 0,low_memory=False)
+data = pd.read_csv('all_data.csv',skiprows=0, header=0, index_col=0, low_memory=False)
 print("Data read complete.")
 data = np.array(data)
-#data = data.flatten('F')
-#print(len(data))
 
-print("Splitting Data for LSTM network input/output sequences...")
 time_steps = 60
+print("Splitting Data for LSTM network input/output sequences...")
 X,y = split_all_data(data,time_steps)
 print("Splitting Done")
+
+# commented out code below is for testing data formatting and visualizing data
 '''
 #for plotting the data purposes
 values = data.values
 groups = [0,1,2,3,4]
-
 i = 1
 pyplot.figure()
 for group in groups:
@@ -130,8 +133,6 @@ for group in groups:
 	i += 1
 pyplot.show()
 
-
-
 # testing data prep results below...
 print(X.shape,y.shape)
 #for i in range(len(X)):
@@ -140,38 +141,43 @@ print(X[len(X)-1],y[len(X)-1])
 # --------------------------------- END OF DATA PREP -------------------------------------- #
 
 # --------------------------------- LSTM NETWORK ------------------------------------------ #
-
-n_features = 4
+# LSTM has already been trained, but for more training, the code below does so
 '''
+n_features = 4
+
 print("Beginning to train LSTM network...")
-print("Hidden Layers = 50, Epochs = 1, Activation = ReLU, Features = 4, Time Steps = 60(5 minutes of 5 second intervals)")
+print("Hidden Layers = 50, Epochs = 6, Activation = ReLU, Features = 4, Time Steps = 60(5 minutes of 5 second intervals)")
 start = time.time()
 model = Sequential()
 model.add(LSTM(50,activation = 'relu',input_shape=(time_steps,n_features)))
-model.add(Dense(1))
-model.compile(optimizer='adam',loss='mse',metrics=['accuracy'])
-model.fit(X,y,validation_split=0.33,epochs=1)
+#model.add(Dense(1))
+model.compile(optimizer='adam',loss='mse',metrics=['mean_squared_error'])
+history = model.fit(X,y,validation_split=0.33,epochs=6)
 model.save('power_prediction_model.h5')
-
 print("Model Metrics")
+
 print("Training Done")
 end = time.time()
 print("Total training time: ")
 print(end-start)
+
 '''
+# number of features of LSTM network input
+n_features = 4
+
 # prediction
 day_19_5_minutes = []
 print("Gathering Day 19 data for Day 20 prediciton...")
-X[len(X)-1][59] = [30,90,1.6,1.2]
+# append manual input (humid,temp,wind,solar) at end of real data
+X[len(X)-1][59] = [35,68,1.3,1.2]
 
 for i in range(len(X)):
-	if i >= (len(X)-1):
+	if i == (5000):
 		day_19_5_minutes.append(X[i])
 
-#day_19_5_minutes.append([[43.8,75.6,1.6,1.2]])
-#print(day_19_5_minutes)
+# load already trained model
 model = load_model("power_prediction_model.h5")
-print("Day 19 last 5 hours info gathered")
+print("Day 19 last 5 minutes info gathered")
 x_input = np.array(day_19_5_minutes)
 print(x_input)
 x_input = x_input.reshape((1,time_steps,n_features))
